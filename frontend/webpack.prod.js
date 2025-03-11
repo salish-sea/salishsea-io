6321dbbd17c8b94804867e325c2cd69cfe02eb6a
Peter Abrahamsen <rainhead@gmail.com>
2025-03-11 16:42:11 -0700

    Switch to vite and express

--- frontend/webpack.prod.js
+++
@@ -1,7 +0,0 @@
-import { merge } from 'webpack-merge';
-import {commonConfig} from './webpack.common.js';
-
-export default merge(commonConfig, {
-  devtool: 'source-map',
-  mode: 'production',
-});
---
+++ src/ol-map.ts
@@ -0,0 +1,67 @@
+import { LitElement, PropertyValues, css, html } from 'lit'
+import { customElement, property, query, queryAll } from 'lit/decorators.js'
+import OpenLayersMap from "ol/Map";
+import View from "ol/View";
+
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat } from 'ol/proj';
+import XYZ from 'ol/source/XYZ';
+
+const center = fromLonLat([-122.450, 47.8]);
+const sphericalMercator = 'EPSG:3857';
+
+// This is a thin wrapper around imperative code driving OpenLayers.
+// Functionality should be progressively factored into new web components.
+// The code is informed by the `openlayers-elements` project, but we avoid taking it as a dependency.
+@customElement('ol-map')
+export class OlMap extends LitElement {
+  public map?: OpenLayersMap = undefined
+
+  @query('#map')
+  public mapElement!: HTMLDivElement
+
+  static styles = css`
+    :host { display: block; }
+    #map { height: 100%; }
+  `
+  public render() {
+    return html`
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ol@v10.4.0/ol.css" type="text/css" />
+      <style>
+      </style>
+      <div id="map"></div>
+      <slot></slot>
+    `;
+  }
+
+  public firstUpdated(_changedProperties: PropertyValues): void {
+    this.map = new OpenLayersMap({
+      layers: [
+        new TileLayer({
+          source: new XYZ({urls: [
+            'https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}',
+            'https://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}',
+          ]}),
+        }),
+        new TileLayer({
+          source: new XYZ({
+            // NB: this source is unmaintained
+            url: "https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Reference/MapServer/tile/{z}/{y}/{x}",
+          }),
+        })
+      ],
+      target: this.mapElement,
+      view: new View({
+        center,
+        projection: sphericalMercator,
+        zoom: 9,
+      }),
+    });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "ol-map": OlMap;
+  }
+}
