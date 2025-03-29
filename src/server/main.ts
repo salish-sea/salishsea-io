69cbd875456b233d9764adbc0c6060e2731cdab1
Peter Abrahamsen <rainhead@gmail.com>
2025-03-28 18:03:17 -0700

    Continue fetching ferries

--- src/server/main.ts
+++ src/server/main.ts
@@ -134,7 +134,7 @@ const loadFerries = async () => {
 await loadRecent();
 await loadFerries();
 setInterval(loadRecent, 1000 * 60 * 5);
-setTimeout(loadFerries, 1000 * 60);
+setInterval(loadFerries, 1000 * 60);
 
 const port = 3131;
 ViteExpress.listen(app, port, () => console.debug(`Listening on port ${port}.`));
