dc3ad75b0cba64bca9ea82308aa9cc11c504e6a5
Peter Abrahamsen <rainhead@gmail.com>
2025-06-13 12:55:54 -0700

    Use REST endpoint to fetch hydrophones

--- src/fetch-hydrophones.ts
+++ src/fetch-hydrophones.ts
@@ -3,52 +3,28 @@ import {writeFileSync} from 'node:fs';
 
 
 const assetPath = 'src/assets/orcasound-hydrophones.geojson';
-const graphqlEndpoint = 'https://live.orcasound.net/graphql';
-// const graphqlEndpoint = 'http://localhost:8888/graphql';
+const endpoint = 'https://live.orcasound.net/api/json/feeds';
 
-console.debug(`Fetching map from ${graphqlEndpoint}`);
-const request = new Request(graphqlEndpoint, {
-  body: JSON.stringify({query:`
-    query feeds($sort: [FeedSortInput]) {
-  feeds(sort: $sort) {
-    id
-    name
-    slug
-    nodeName
-    latLng {
-      lat
-      lng
-    }
-    imageUrl
-    thumbUrl
-    mapUrl
-    bucket
-    online
-  }
-}
-    `}),
-  headers: {
-    'Content-Type': 'application/json; charset=utf-8',
-  },
-  method: 'POST',
-});
-const response = await fetch(request);
+console.debug(`Fetching map from ${endpoint}`);
+const response = await fetch(endpoint);
 if (response.status !== 200)
   throw `Error fetching endpoints: ${response.statusText}`;
 const body: Payload = await response.json();
 
-const features: Feature[] = body.data.feeds.map(feed => {
-  const {lat, lng} = feed.latLng;
-  return {
-    id: `orcasound:${feed.id}`,
-    geometry: {type: 'Point', coordinates: [lng, lat]},
-    properties: {
-      kind: 'Hydrophone',
-      url: `https://live.orcasound.net/listen/${feed.slug}`,
-    },
-    type: 'Feature',
-  };
-});
+const features: Feature[] = body.data
+  .filter(feed => feed.attributes.visible)
+  .map(feed => {
+    const {lat, lng} = feed.attributes.lat_lng;
+    return {
+      id: `orcasound:${feed.id}`,
+      geometry: {type: 'Point', coordinates: [lng, lat]},
+      properties: {
+        kind: 'Hydrophone',
+        url: `https://live.orcasound.net/listen/${feed.attributes.slug}`,
+      },
+      type: 'Feature',
+    };
+  });
 const collection: FeatureCollection = {
   type: 'FeatureCollection',
   features,
@@ -56,16 +32,18 @@ const collection: FeatureCollection = {
 writeFileSync(assetPath, JSON.stringify(collection) + '\n');
 
 type Feed = {
+  attributes: {
+    name: string;
+    visible: boolean;
+    slug: string;
+    lat_lng: {
+      lng: number;
+      lat: number;
+    };
+  };
   id: string;
-  imageUrl: string;
-  latLng: {lat: number; lng: number};
-  mapUrl: string;
-  name: string;
-  online: boolean;
-  slug: string;
+  type: 'feed';
 };
 type Payload = {
-  data: {
-    feeds: Feed[];
-  };
+  data: Feed[];
 };
