6167a170b0527f8e05624f32639e9da3ff711194
Peter Abrahamsen <rainhead@gmail.com>
2025-07-04 14:16:14 -0700

    Add missing constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+import type { Extent } from "./types.ts";
+
+// https://github.com/salish-sea/acartia/wiki/1.-Context-for-SSEMMI-&-Acartia#spatial-boundaries-related-to-acartia
+export const acartiaExtent: Extent = [-136, 36, -120, 54];
+export const srkwExtent: Extent = [-125.5, 36, -122, 54];
+export const salishSeaExtent: Extent = [-126, 47, -122, 50.5];
+export const salishSRKWExtent: Extent = [-124, 47, -122, 49.5];
