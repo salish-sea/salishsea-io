06c4485238d43381679fd9f17991a681b886be89
Peter Abrahamsen <rainhead@gmail.com>
2025-09-06 14:49:33 -0700

    Seed some inaturalist observations

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,6 +1,17 @@
 INSERT INTO happywhale.species (code, name, plural, scientific)
 SELECT code, name, plural, scientific FROM happywhale.fetch_species_config();
 
+select *
+FROM inaturalist.fetch_observation_page(
+  current_date - 30,
+  current_date,
+  gis.ST_MakeBox2D(gis.ST_Point(-136, 36), gis.ST_Point(-120, 54)),
+  array[152871],
+  1,
+  1),
+inaturalist.upsert_observation_page(results) ups;
+
+
 INSERT INTO maplify.sightings
 SELECT fetched.* FROM
   gis.ST_MakeBox2D(gis.ST_Point(-136, 36), gis.ST_Point(-120, 54)) AS bbox,
