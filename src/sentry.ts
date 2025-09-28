b1c48d6263088b97a8485fe703e98ecd633212a1
Peter Abrahamsen <rainhead@gmail.com>
2025-09-28 16:29:38 -0700

    Reenable Sentry tracing

--- src/sentry.ts
+++ src/sentry.ts
@@ -6,6 +6,8 @@ export const sentryClient = new BrowserClient({
   dsn: "https://56ce99ce80994bab79dab62d06078c97@o4509634382331904.ingest.us.sentry.io/4509634387509248",
   transport: makeFetchTransport,
   stackParser: defaultStackParser,
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
   integrations: [
     browserTracingIntegration({
       shouldCreateSpanForRequest: url => !url.startsWith(`${import.meta.env.VITE_SUPABASE_URL}/rest`),
