e459e4a9daf48f9a3689e68da18f8d292a06b801
Peter Abrahamsen <rainhead@gmail.com>
2025-09-28 17:10:03 -0700

    Tidy

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,7 +6,6 @@ interface ViteTypeOptions {
 
 interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
-  readonly VITE_GOOGLE_CLIENT_ID: string
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_KEY: string;
 }
