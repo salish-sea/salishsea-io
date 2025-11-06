f5bd6526738ed18d4e7d4f030a3358af0b1259a0
Peter Abrahamsen <rainhead@gmail.com>
2025-11-06 10:45:45 -0800

    Make gsi-callback Typescript

---
+++ src/gsi-callback.ts
@@ -0,0 +1,29 @@
+// Global Google Sign In callback defined early so GSI script can invoke it.
+
+import SalishSea from "./salish-sea.ts";
+
+// It queues credentials until <salish-sea> is upgraded and ready.
+(function(){
+  const GLOBAL = window;
+  GLOBAL.handleSignInWithGoogle = (response: {credential: string}) => {
+    GLOBAL.__pendingGSIResponses = GLOBAL.__pendingGSIResponses || [];
+    const el = document.querySelector('salish-sea');
+    if (el && el instanceof SalishSea) {
+      try {
+        el.receiveIdToken(response.credential);
+      } catch (e) {
+        GLOBAL.__pendingGSIResponses.push(response.credential);
+        throw e;
+      }
+    } else {
+      GLOBAL.__pendingGSIResponses.push(response.credential);
+    }
+  };
+})();
+
+declare global {
+  interface Window {
+    handleSignInWithGoogle: (response: {credential: string}) => void;
+    __pendingGSIResponses?: string[];
+  }
+}
