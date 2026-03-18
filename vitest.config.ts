1b1643535231aa1df386a80dfd58326d61240c3b
Peter Abrahamsen <rainhead@gmail.com>
2026-03-18 11:37:50 -0700

    Fix test runs

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,6 @@ export default defineConfig(({ mode }) => ({
     test: {
         // mode defines what ".env.{mode}" file to choose if exists
         env: loadEnv(mode, process.cwd(), ''),
+        exclude: ['infra/**', 'node_modules/**'],
     },
 }));
